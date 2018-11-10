9b1bd53debe5e4172645fb197cd6c62c7a632218
Philip Ross <philiplross@gmail.com>
2018-11-10 13:48:43 -0800

    Update app/data/friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,12 +11,15 @@
 
 
 
-// this is the are to put in an array of all the friends in the database to store and recall matches and values that friends has
+// this is the are to put in an array of all the friends in the database to 
+// store and recall matches and values that friends has
 
 
+// after the array is inserted to export module to make a route to display 
+// the friend that matches
 
-// after the array is inserted to export module to make a route to display the friend that matches
 
 // the object need to be export into JSON with a tree of objects
+
 
 // if there are any errors to console log the 500 sever error and alert user
