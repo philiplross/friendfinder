33130505d67e92749447a97252d3c09f4a3e6e0a
Philip Ross <philiplross@gmail.com>
2018-11-10 13:47:40 -0800

    psudo coding on friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,22 @@
-var friends = [];
-var friendsLength = friends.length;
-
-// implement an array of friends to pull and store to and from
-
-
-for (var i = 0; i < friends.length; i++);
+// var friends = [];
+// var friendsLength = friends.length;
 
 
 
 
+// // implement an array of friends to pull and store to and from
 
-// [
-// {
-// "name": ,
-// "photo": ,
-// "scores":[
 
-// ]
+// for (var i = 0; i < friends.length; i++);
 
-// }]
+
+
+// this is the are to put in an array of all the friends in the database to store and recall matches and values that friends has
+
+
+
+// after the array is inserted to export module to make a route to display the friend that matches
+
+// the object need to be export into JSON with a tree of objects
+
+// if there are any errors to console log the 500 sever error and alert user
